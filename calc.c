5878cc31a79b81834f2770d16d7013bac4cacaa2
Mohamed Athif <Mohamed Athif>
2017-02-25 14:20:25 +0530

    menu print

---
+++ calc.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+    int c;
+    do
+    {
+        printf("Calculator");
+        printf("1:Add,2:Sub,3:Mul,4:Div,5:Exit");
+        printf("Enter choice");
+        scanf("%d",$c);
+    } while(c!=5);
+}
