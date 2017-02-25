b1bc04a9a69ce786907eb4eb41e02819b00b8b3c
Mohamed Athif <Mohamed Athif>
2017-02-25 14:43:17 +0530

    addition

--- calc.c
+++ calc.c
@@ -1,12 +1,22 @@
 #include<stdio.h>
 void main()
 {
-    int c;
+    int c,n1,n2,sum;
     do
     {
         printf("Calculator");
         printf("1:Add,2:Sub,3:Mul,4:Div,5:Exit");
         printf("Enter choice");
-        scanf("%d",$c);
+        scanf("%d",&c);
+        switch(c)
+        {
+            case 1 : printf("Addition");
+                        printf("Enter 2 Nos :");
+                        scanf("%d",&n1);
+                        scanf("%d",&n2);
+                        sum=n1+n2;
+                        printf("sum is %d",sum);
+                        break;
+        }
     } while(c!=5);
 }
